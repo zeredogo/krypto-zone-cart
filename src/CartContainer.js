5d52e175c15fc75280e41c286936d8869d8ccef0
zeredogo <zeredogo@gmail.com>
2022-08-14 23:44:28 +0100

    updating commit

--- src/CartContainer.js
+++ src/CartContainer.js
@@ -3,8 +3,18 @@ import CartItem from './CartItem'
 import { useGlobalContext } from './context'
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext()
- 
+  const { cart, total } = useGlobalContext()
+  if (cart.length === 0) {
+    return (
+      <section className='cart'>
+        {/* cart header */}
+        <header>
+          <h2>your bag</h2>
+          <h4 className='empty-cart'>is currently empty</h4>
+        </header>
+      </section>
+    )
+  }
   return (
     <section className='cart'>
       {/* cart header */}
@@ -22,7 +32,7 @@ const CartContainer = () => {
         <hr />
         <div className='cart-total'>
           <h4>
-            total <span>$0.00</span>
+            total <span>${total}</span>
           </h4>
         </div>
         <button
