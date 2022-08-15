af7c8ae480578aee5470b920ebf059f4377ea971
zeredogo <zeredogo@gmail.com>
2022-08-15 01:20:08 +0100

    updating commit

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,13 @@
+import CartItem from "./CartItem";
 
 const reducer = (state, action) => {
     if(action.type === 'CLEAR_CART') {
         return {...state, cart: []}
     }
     if(action.type === 'REMOVE') {
-        return {...state, cart:}
+        return {...state, cart: state.cart.filter((CartItem) => CartItem.id
+            !== action.payload),
+        }
     }
     return state
 }
