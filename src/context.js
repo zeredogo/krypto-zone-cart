08b4537657de6aca442738c85f512ea3ddc5d0de
zeredogo <zeredogo@gmail.com>
2022-08-17 01:28:02 +0100

    updating commit

--- src/context.js
+++ src/context.js
@@ -32,6 +32,19 @@ const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({type: 'DECREASE', payload: id })
   }
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING' })
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({ type: 'DISPLAY_ITEMS', payload: cart })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+
   useEffect(()=> {
     dispatch({  type: 'GET_TOTALS' })
   }, [state.cart])
--- src/reducer.js
+++ src/reducer.js
@@ -47,6 +47,12 @@ const reducer = (state, action) => {
 
         return {...state, total, amount}
     }
+    if (action.type === 'LOADING' ) {
+        return {...state, loading: true }
+    }
+    if (action.type === 'DISPLAY_ITEMS' ) {
+        return {...state, cart: action.payload, loading: false }
+    }
 
     return state
 }
