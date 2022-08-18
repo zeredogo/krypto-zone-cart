473e64fc2a13b0c9f71df14bfa1be0d4bded629e
zeredogo <zeredogo@gmail.com>
2022-08-18 23:39:28 +0100

    updating commit

--- src/reducer.js
+++ src/reducer.js
@@ -53,13 +53,18 @@ const reducer = (state, action) => {
     if (action.type === 'DISPLAY_ITEMS' ) {
         return {...state, cart: action.payload, loading: false }
     }
-    if (action.type === 'TOGGLR_AMOUNT') {
+    if (action.type === 'TOGGLE_AMOUNT') {
         let tempCart = state.cart.map((cartItem) => {
             if(cartItem.id === action.payload.id) {
-
+              if(action.payload.type === 'inc'){
+                return {...cartItem, amount: cartItem.amount + 1 }
+              }
+              if(action.payload.type === 'dec'){
+                return {...cartItem, amount: cartItem.amount - 1 }
+              }
             }
             return cartItem
-        });
+        }).filter((CartItem) => CartItem.amount !== 0)
         return {...state, cart: tempCart}
     }
 
